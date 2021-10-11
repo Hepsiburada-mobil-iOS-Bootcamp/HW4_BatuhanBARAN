4b39e1c34d24605ee68550b71be182bc3678b174
batuhanbaran <fb_batuhan_baran@hotmail.com>
2021-10-11 21:43:00 +0300

    Initial Commit

---
+++ HW4_BatuhanBaran/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW4_BatuhanBaran
+//
+//  Created by Batuhan BARAN on 11.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
